ea5bc5408610419cd8a11ebdee32258fccce6a92
Lauro Correa Junior <lauro.junior@fundatecserver.com.br>
2020-02-10 17:45:51 -0300

    atualizado o laravel para o 6

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -31,6 +31,8 @@ class Handler extends ExceptionHandler
      *
      * @param  \Exception  $exception
      * @return void
+     *
+     * @throws \Exception
      */
     public function report(Exception $exception)
     {
@@ -42,14 +44,12 @@ class Handler extends ExceptionHandler
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Exception
      */
     public function render($request, Exception $exception)
     {
-        // if ($exception instanceof \Spatie\Permission\Exceptions\UnauthorizedException) {
-        //     return response()->json(['User have not permission for this page access.']);
-        // }
-
         return parent::render($request, $exception);
     }
 }
